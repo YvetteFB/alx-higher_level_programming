7c960b7a8370c494be7b19c9cc5564dc235733bb
YvetteFB <yvettefionabaraza@gmail.com>
2022-06-28 00:35:04 -0700

    task 7-9

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/bin/bash/python3
+str = "Python is an interprated, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] +str[:6]
+print(str)
