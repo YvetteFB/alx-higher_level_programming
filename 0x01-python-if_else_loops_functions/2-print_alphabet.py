8f97dd11c855e7bb77e8b03322f7da41c85aeedd
YvetteFB <yvettefionabaraza@gmail.com>
2022-07-04 06:31:00 -0700

    alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-n=26
-res =""
-for idx in range(97, 97 + n):
-    res = res + chr(idx)
-
-print(str(res))
+for character in ranger(97, 123):
+    print("{:c}".format(character), end ="")
